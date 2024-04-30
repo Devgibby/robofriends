1eca6eb96f5ca2d418639a1e4818fa638410487f
Devgibby <gideonadebisi01@gmail.com>
2024-04-30 11:17:53 -0700

    starting project

---
+++ src/Hello.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+// import './Hello.css'
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <p>Welcome to react</p>
+
+            </div>
+
+        )
+        
+    }
+
+}
+
+export default Hello; 
